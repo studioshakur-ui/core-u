f05654f84863f1042209ac8fc81c3264fd11bc49
studioshakur-ui <studio.shakur@gmail.com>
2025-09-16 20:48:17 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import { HashRouter } from "react-router-dom"
-import App from "./App.jsx"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { HashRouter } from "react-router-dom";
+import App from "./App.jsx";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -9,4 +10,4 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <App />
     </HashRouter>
   </React.StrictMode>
-)
+);
