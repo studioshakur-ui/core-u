a56fdbd09656b67acbf0e3c18400c7a1ba993c9d
studioshakur-ui <studio.shakur@gmail.com>
2025-09-16 19:45:42 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
 export default defineConfig({
-  plugins: [react()],
-  define: { 'process.env': {} },
-  optimizeDeps: { include: ['exceljs','react-window','html2canvas','jspdf'] },
+  plugins: [react()],               // 👈 indispensable
 })
