5c1db928e16988a18a6ac0f899ef67e1ade57fb4
studioshakur-ui <studio.shakur@gmail.com>
2025-09-16 20:48:02 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  esbuild: {
-    jsx: "automatic",  // 👈 important
-  },
+  esbuild: { jsx: "automatic" },
+  optimizeDeps: { include: ["react", "react-dom", "react-router-dom"] },
+  build: { sourcemap: true },
 });
