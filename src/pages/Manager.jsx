6f29ab0d036458a4b75f3ab068db349e3feceaa0
studioshakur-ui <studio.shakur@gmail.com>
2025-09-17 11:59:12 +0200

    Create Manager.jsx

---
+++ src/pages/Manager.jsx
@@ -0,0 +1,8 @@
+export default function Manager() {
+  return (
+    <div style={{ padding: 16 }}>
+      <h1 style={{ margin: 0 }}>Manager</h1>
+      <p>Zone Manager prête. Tu pourras brancher planification, organigramma, etc.</p>
+    </div>
+  );
+}
