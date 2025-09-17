f5bb7b56f6ed8158b0fbe1694d8a8b406ca64405
studioshakur-ui <studio.shakur@gmail.com>
2025-09-17 11:38:17 +0200

    Update supabaseClient.js

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -7,7 +7,7 @@ const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
     persistSession: true,
-    detectSessionInUrl: true, // important pour magic link/OAuth
-    flowType: "pkce",         // OK aussi pour magic link, gère callback
+    detectSessionInUrl: true,
+    flowType: "pkce",
   },
 });
