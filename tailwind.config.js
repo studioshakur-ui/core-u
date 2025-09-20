cd248950128a0ef60fd9ae540cb2dd7d373276fb
studioshakur-ui <studio.shakur@gmail.com>
2025-09-20 14:56:13 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,27 @@
+// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
-  theme: { extend: {} },
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        core: {
+          50:  "#f7f5ff",
+          100: "#eee9ff",
+          200: "#dac8ff",
+          300: "#c3a0ff",
+          400: "#a86dff",
+          500: "#8f3dff",
+          600: "#7a25f2",
+          700: "#671fd1",
+          800: "#4d18a0",
+          900: "#37126f"
+        }
+      }
+    }
+  },
   plugins: [],
 };
