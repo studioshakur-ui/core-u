2db91ee506ee6af453d1a7f134aa5888ac509a61
studioshakur-ui <studio.shakur@gmail.com>
2025-09-20 09:45:35 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,24 @@
+// vite.config.js
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
+import path from "node:path";
+
 export default ({ mode }) => {
   loadEnv(mode, process.cwd(), "");
   return defineConfig({
     plugins: [react()],
-    resolve: { alias: { "@": path.resolve(__dirname, "src") } },
-    build: { outDir: "dist", target: "es2019", sourcemap: true,
-      rollupOptions: { output: { manualChunks: { vendor: ["react", "react-dom", "react-router-dom"] } } } }
+    resolve: {
+      alias: { "@": path.resolve(process.cwd(), "src") },
+    },
+    build: {
+      outDir: "dist",
+      target: "es2019",
+      sourcemap: true,
+      rollupOptions: {
+        output: {
+          manualChunks: { vendor: ["react", "react-dom", "react-router-dom"] },
+        },
+      },
+    },
   });
 };
