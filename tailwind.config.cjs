4f0b91ea02733cf150511c4913cf2c237f4c3d0f
studioshakur-ui <studio.shakur@gmail.com>
2025-09-21 14:37:27 +0200

    Delete tailwind.config.cjs

--- tailwind.config.cjs
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  darkMode: ['class'],
-  content: ['./index.html','./src/**/*.{ts,tsx}'],
-  theme: { extend: { borderRadius: { '2xl':'1.25rem' }, boxShadow: { soft:'0 8px 28px rgba(0,0,0,.12)' } } },
-  plugins: [],
-}
