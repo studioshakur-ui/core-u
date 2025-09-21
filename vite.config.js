c85cee74aebb9f51479724dbd807921935e0c19c
studioshakur-ui <studio.shakur@gmail.com>
2025-09-22 01:32:41 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: 'es2019' // suffisant pour Netlify/Chrome/Edge récents
+  }
 })
