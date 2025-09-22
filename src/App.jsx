512f74fc002e3cb56e50db22af901ba8ab6f01f1
studioshakur-ui <studio.shakur@gmail.com>
2025-09-22 16:27:00 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,32 @@
 import React from 'react'
-import { Routes, Route, Navigate, Link } from 'react-router-dom'
-import Capo from './pages/Capo'
-import Home from './pages/Home'
-import Login from './pages/Login'
+import { Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar.jsx'
-import Protected from './components/Protected'
+import Home from './pages/Home.jsx'
+import Login from './pages/Login.jsx'
+import Capo from './pages/Capo.jsx'
+import { RequireAuth, RoleRouter } from './components/AuthGate.jsx'
 
 export default function App(){
   return (
-    <div className="min-h-screen bg-slate-50 text-slate-900">
+    <>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
 
-        {/* Capo seulement, pas d’URL manager/direzione */}
-        <Route path="/capo" element={
-          <Protected><Capo /></Protected>
-        } />
+        <Route
+          path="/capo"
+          element={
+            <RequireAuth>
+              <RoleRouter>
+                <Capo />
+              </RoleRouter>
+            </RequireAuth>
+          }
+        />
 
-        <Route path="*" element={<Navigate to="/" replace />} />
+        <Route path="*" element={<Home />} />
       </Routes>
-    </div>
+    </>
   )
 }
