2c7615a7de381752f9fa9706da77055a059408b2
studioshakur-ui <studio.shakur@gmail.com>
2025-09-23 15:24:20 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
@@ -7,7 +6,10 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
+      "@": path.resolve(__dirname, "./src")
+    }
   },
+  optimizeDeps: {
+    include: ["xlsx-populate"]
+  }
 });
