0dcc0d9bb27fac81ca3597d6ef9e33c9d3bb3347
studioshakur-ui <studio.shakur@gmail.com>
2025-09-23 15:33:51 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App.jsx'
-import './index.css'
-createRoot(document.getElementById('root')).render(
-  <React.StrictMode><BrowserRouter><App/></BrowserRouter></React.StrictMode>
-)
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css"; // ⬅️ important pour Tailwind
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
